c862deb74ccb4cc22c0de1228ee5a5ce90debc4a
Durbekuz <durbekkanalbekov74@gmail.com>
2025-12-14 22:25:12 +0500

    Change

---
+++ School.Doman/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Doman.Models;
+
+public class Student
+{
+    public Student(string firstName,string lastName,int age,int id)
+    {
+       FirstName = firstName;
+       LastName = lastName;
+        FullName = $"{firstName} {lastName}";
+        Age = age;
+        StudentId = id;
+    }
+    
+    public string FullName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public int StudentId { get; set;
+}
---
+++ School/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using School.Doman.Models;
+
+
+
+Student add  = new Student("Akbar","Aliyev", 43, 8543875);
+Console.WriteLine($"Fullname : {add.FullName}");
+Console.WriteLine($"Name : {add.FirstName}");
+Console.WriteLine($"Lastname : { add.LastName}");
+
+Teacher = new Student("Akbar", "Aliyev", 43, 8543875);
+Console.WriteLine($"Fullname : {add.FullName}");
+Console.WriteLine($"Name : {add.FirstName}");
+Console.WriteLine($"Lastname : {add.LastName}");
